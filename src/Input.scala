fb726c8153d41b929844b22a02a1e23fde73d7ec
xorcist-part <parthjshah95@gmail.com>
2016-07-21 15:59:05 +0530

    changed input

--- src/Input.scala
+++ src/Input.scala
@@ -1,62 +1,46 @@
 /**
   * Created by cheekati on 7/21/2016.
   */
-object Input
-
-{
+object Input {
 
   def getusername() {
     print("please Enter user Name")
     var X: String = io.StdIn.readLine()
     return X
   }
-  print("please Enter Pin")
-  var Y :Int = io.StdIn.readInt()
+
   def getpin() {
+    print("please Enter Pin")
+    var Y: Int = io.StdIn.readInt()
     return Y
   }
-  print("please select an option")
-  var i :Int =io.StdIn.readInt()
 
-  if(Y ==PIN)
-  {
-    print("please select an option")
-    def Option(i : Int): Unit = {
 
-      if (0 < i && 6 > i) {
+  def Option(): Int = {
+    println("please select an option:\ntype 1 to view account balance\ntype 2 to deposit\ntype 3 to withdraw\ntype 4 to transfer to another account" )
+    var i: Int = io.StdIn.readInt()
+    if (0 < i && 5 > i) {
 
       if (i == 1) {
-        print("You can view your account now")
+        println("You can view your account balance now")
         return 1
       }
-      if (i == 2)
-      {
-        print("You can Deposit into account now")
+      if (i == 2) {
+        println("You can Deposit into account now")
         return 2
       }
-      if (i == 3)
-      {
-        print("You can Withdraw From account now")
+      if (i == 3) {
+        println("You can Withdraw From account now")
         return 3
       }
-      if (i == 4)
-      {
-        print("You can check your balance now")
+      if (i == 4) {
+        println("You can check your balance now")
         return 4
       }
-    }
-
       else {
-        print("wrong pin/user Name reenter the correct one")
+        println("please type a valid number")
+        Option()
       }
-      }
-
-
-
-    print("please select an option")
-    Option(i)
-
-
-
-}
+    }
+  }
 }
