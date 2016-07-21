3e7ee6d83789bade58a6b758a2488aae7324a037
Cheekati <akhil.cheekati@here.com>
2016-07-21 14:36:28 +0530

    CHanged

--- src/Account1.scala
+++ src/Account1.scala
@@ -2,5 +2,33 @@
   * Created by cheekati on 7/21/2016.
   */
 object Account1 {
+  var amount: BigInt = 0
+  var user: String = "user2"
+  var PIN: Int = 9999
+  var Z = 0
+
+  def view_amount(): BigInt = {
+    return amount
+  }
+
+  def deposit(x: BigInt): BigInt = {
+    amount += x
+    amount
+  }
+
+  def withdraw(x: BigInt): BigInt = {
+    amount -= x
+    amount
+  }
+
+  def Change_PIN(x: Int): String = {
+    if (x >= 1000 && x <= 9999) {
+      PIN = x
+      return "PIN has been changed"
+    }
+    else{
+      return "Invalid PIN format. Please enter a number between 1000 and 9999"
+    }
+  }
 
 }
--- src/Input.scala
+++ src/Input.scala
@@ -2,36 +2,41 @@
   * Created by cheekati on 7/21/2016.
   */
 object Input {
-  if()
-  {
-   print("please select an option")
-    Options();
+  var Z = 0
 
-  }
-  else
-  {
-    print("wrong pin reenter the correct one")
-  }
-def Options(i : Int): Unit ={
-  if(1 < i && 6 > i) {
+  
 
-  }
-  if(i == 1)
-    {
-      view account()
+  {
+    print("please select an option")
+    def Options(i : Int): Unit = {
+      if (1 < i && 6 > i) {
+
+      }
+      if (i == 1) {
+        print("You can view your account now")
+        view account()
+      }
+      if (i == 2) {
+        print("YOu can Deposit into account now")
+        Deposit()
+      }
+      if (i == 3) {
+        print("YOu can Withdraw From account now")
+        Withdraw()
+      }
+      if (i == 4) {
+        print("You can Withdraw From account now")
+        Transfer()
+      }
+      else {
+        print("wrong pin reenter the correct one")
+      }
+      print("please select an option")
+      Options(i: Int);
     }
-  if(i == 2)
-  {
-   Deposit()
-  }
-  if(i == 3)
-  {
-    Withdraw()
-  }
-  if(i == 4)
-  {
-    Transfer()
-  }
+
+
+
 
 
 }
